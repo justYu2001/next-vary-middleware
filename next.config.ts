5898f5df9dc0f9b4caf6c279c3b84756b204ec0d
justYu2001 <jack4781012@gmail.com>
2025-11-17 19:36:32 +0800

    try set vary via config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  headers: async () => ([
+      {
+          source: "/post/:id",
+          headers: [
+              {
+                  key: "Vary",
+                  value: "Accept-Language"
+              },
+              {
+                  key: "x-custom",
+                  value: "test"
+              }
+          ]
+      }
+  ])
 };
 
 export default nextConfig;
